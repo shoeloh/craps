cd53f94f482d2f7ffc8bb3005ee2555e718c9cc3
shoeloh <shaneazabel@outlook.com>
2019-10-20 22:14:04 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,84 @@
+﻿README.txt
+
+Program was written in R
+
+No compiling of R … its an interpreted language. Just run the program file in R and execute the function craps()
+
+Program file is craps.R
+
+Output file is Craps_Results.txt
+
+In intial trials, averaged over the 5 rounds all strategies were loosing strategies. Even wager strategy (strategy 1) lost the least amount of money on average. Reverse Martingale strategy (strategy 3) lost the most on average. Martingale strategy (strategy 2) had the highest return on any single round ($1600).
+
+Best strategy was to not play.
+
+Sample Output:
+
+ Start of Round  1 
+Round		 Game			 Strategy 1	 Strategy 2	 Strategy 3 
+1 		 1 			 1100 		 1100 		 1100 
+1 		 2 			 1000 		 1000 		 900 
+1 		 3 			 900 		 800 		 800 
+1 		 4 			 1000 		 1200 		 900 
+1 		 5 			 900 		 1100 		 700 
+1 		 6 			 1000 		 1300 		 800 
+1 		 7 			 1100 		 1400 		 1000 
+1 		 8 			 1000 		 1300 		 600 
+1 		 9 			 1100 		 1500 		 700 
+1 		 10 			 1000 		 1400 		 500 
+
+ Start of Round  2 
+Round		 Game			 Strategy 1	 Strategy 2	 Strategy 3 
+2 		 1 			 900 	 	 900 		 900 
+2 		 2 			 1000 		 1100 		 1000 
+2 		 3 			 1100 		 1200 		 1200 
+2 		 4 			 1200 		 1300 		 1600 
+2 		 5 			 1100 		 1200 		 800 
+2 		 6 			 1200 		 1400 		 900 
+2 		 7 			 1300 		 1500 		 1100 
+2 		 8 			 1200 		 1400 		 700 
+2 		 9 			 1100 		 1200 		 600 
+2 		 10 			 1000 		 800 		 500 
+
+ Start of Round  3 
+Round		 Game			 Strategy 1		 Strategy 2		 Strategy 3 
+3 		 1 			 900 		 	 900 		 	 900 
+3 		 2 			 800 		 	 700 		 	 800 
+3 		 3 			 700 		 	 300 		 	 700 
+3 		 4 			 600 		 	 0 		 	 600 
+3 		 5 			 500 		 	 0 		 	 500 
+3 		 6 			 400 		 	 0 		 	 400 
+3 		 7 			 300 		 	 0 		 	 300 
+3 		 8 			 400 		 	 0 		 	 400 
+3 		 9 			 300 		 	 0 		 	 200 
+3 		 10 			 400 		 	 0 		 	 300 
+
+ Start of Round  4 
+Round		 Game			 Strategy 1	 Strategy 2	 Strategy 3 
+4 		 1 			 1100 		 1100 		 1100 
+4 		 2 			 1200 		 1200 		 1300 
+4 		 3 			 1300 		 1300 		 1700 
+4 		 4 			 1200 		 1200 		 900 
+4 		 5 			 1100 		 1000 		 800 
+4 		 6 			 1000 		 600 	 	 700 
+4 		 7 			 900 		 0 		 600 
+4 		 8 			 1000 		 0 		 700 
+4 		 9 			 900 		 0 		 500 
+4 		 10 			 800 		 0 		 400 
+
+
+
+
+ Start of Round  5 
+Round		 Game			 Strategy 1	 Strategy 2	 Strategy 3 
+5 		 1 			 900 		 900 	       	 900 
+5 		 2 			 800 		 700 		 800 
+5 		 3 			 900 		 1100 		 900 
+5 		 4 			 1000 		 1200 		 1100 
+5 		 5 			 1100 		 1300 		 1500 
+5 		 6 			 1000 		 1200 		 700 
+5 		 7 			 900 		 1000 		 600 
+5 		 8 			 1000 		 1400 		 700 
+5 		 9 			 1100 		 1500 		 900 
+5 		 10 			 1200 		 1600 		 1300 
+
---
+++ craps.R
@@ -0,0 +1,261 @@
+# MIT License
+#
+# Copyright (c) 2019 shoeloh 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# =============================================================================
+#
+# Simulation of the game of craps 
+# Testing 3 different craps strategies: 
+# Even wager, Martingale, Reverse Martingale 
+# 
+###############################################################################
+
+craps <- function(){
+	#Initialize simulation parameters
+	#$1000 starting cash, $100 initial bet, 5 rounds, each round 10 games max
+	starting_cash <- c(1000)
+	first_bet <- c(100)
+	max_rounds <- c(5)
+	max_games <- c(10)
+	
+	#Set craps parameters
+	field <- c(2,3,12)
+	wins <- c(7,11)
+	point <- c(4, 5, 6, 8, 9, 10)
+	
+	#Open file for writing
+	outFile <- file("Craps_Results.txt","w")
+	
+	for(i in 1:max_rounds){
+		#Initialize paramaters within a round
+		current_round <- i
+		current_cash_1 <- current_cash_2 <- current_cash_3 <- starting_cash
+		bet_1 <- bet_2 <- bet_3 <- first_bet
+		num_games_1 <- num_games_2 <- num_games_3 <- c(0)
+		
+		#Initialize round results printout
+		cat("\n","Start of Round ",current_round,"\n")
+		cat("Round\t\tGame\t\t\tEvenWager\tMartingale\tReverseMartingale", "\n")	
+		
+		for(j in 1:max_games){
+			#Initialize parameters within a game. game_flag switches when the roll wins/looses
+			current_iteration <- j
+			game_flag <- c(0)
+			
+			#Update games played counter
+			if(bet_1 != 0){
+				num_games_1 <- num_games_1 +1
+			}
+			if(bet_2 != 0){
+				num_games_2 <- num_games_2 +1
+			}
+			if(bet_3 != 0){
+				num_games_3 <- num_games_3 +1
+			}
+			
+			#Game continues until all strategies are out of cash or roll wins/looses
+			while( ((current_cash_1 > 0) | (current_cash_2 > 0) | (current_cash_3 > 0)) & game_flag==0){
+				#Initialize curent point
+				current_point <- c(0)
+				#Roll dice
+				first_die <- sample(1:6,1)
+				second_die <- sample(1:6,1)
+				roll <- first_die + second_die
+				
+				#Case if roll is a winner
+				if(roll %in% wins){
+					#Add cash based on bet
+					current_cash_1 <- current_cash_1 + bet_1
+					current_cash_2 <- current_cash_2 + bet_2
+					current_cash_3 <- current_cash_3 + bet_3
+					
+					#Update bets based on strategy
+					#Even wager strategy
+					if(current_cash_1 == 0 ){
+						bet_1 <- c(0)
+					}else{
+						bet_1 <- first_bet
+					}
+					#Martingale strategy
+					if(current_cash_2 == 0){
+						bet_2 <- c(0)
+					}else{
+						bet_2 <- first_bet
+					}
+					#Reverse Martingale strategy
+					if(current_cash_3 == 0){
+						bet_3<- c(0)
+					}else{
+						#If there is not enough cash to double the bet, bet remaining cash
+						if(current_cash_3<bet_3*2){
+							bet_3 <- current_cash_3
+						}else{
+							bet_3 <- bet_3*2
+						}
+					}
+					
+					#Switch game_flag
+					game_flag <- c(1)
+				}
+				
+				#Case if roll is a looser
+				if(roll %in% field){
+					#Reduce cash based on bet
+					current_cash_1 <- current_cash_1 - bet_1
+					current_cash_2 <- current_cash_2 - bet_2
+					current_cash_3 <- current_cash_3 - bet_3
+					
+					#Update bets based on strategy
+					#Even wager strategy
+					if(current_cash_1 == 0 ){
+						bet_1 <- c(0)
+					}else{
+						bet_1 <- first_bet
+					}
+					#Martingale strategy
+					if(current_cash_2 == 0 ){
+						bet_2 <- c(0)
+					}else{
+						#If there is not enough cash to double the bet, bet remaining cash
+						if(current_cash_2<bet_2*2){
+							bet_2 <- current_cash_2
+						}else{
+							bet_2 <- bet_2*2
+						}
+					}
+					#Reverse Martingale strategy
+					if(current_cash_3 == 0 ){
+						bet_3 <- c(0)
+					}else{
+						bet_3 <- first_bet
+					}
+					
+					#Switch game flag
+					game_flag <- c(1)
+				}
+				
+				#Case if roll sets a point
+				if(roll %in% point){
+					#Set the point
+					current_point <- roll
+					#Initialize point roll
+					z <- c(0)
+					
+					#Keep rolling until the point is hit or a 7 is rolled
+					while(z != current_point && z != 7){
+						#Roll the dice
+						x <- sample(1:6,1)
+						y <- sample(1:6,1)
+						z <- x + y
+						
+						#Case where point is rolled
+						if(z == current_point){			
+							#Add cash based on bet
+							current_cash_1 <- current_cash_1 + bet_1
+							current_cash_2 <- current_cash_2 + bet_2
+							current_cash_3 <- current_cash_3 + bet_3
+							
+							#Update bets based on strategy
+							#Even wager strategy
+							if(current_cash_1 == 0){
+								bet_1 <- c(0)
+							}else{
+								bet_1 <- first_bet
+							}
+							#Martingale strategy
+							if(current_cash_2 == 0){
+								bet_2 <- c(0)
+							}else{
+								bet_2 <- first_bet
+							}
+							#Reverse Martingale strategy
+							if(current_cash_3 == 0){
+								bet_3 <- c(0)
+							}else{
+								#If there is not enough cash to double the bet, bet remaining cash
+								if(current_cash_3<bet_3*2){
+									bet_3 <- current_cash_3
+								}else{
+									bet_3 <- bet_3*2
+								}
+							}
+						}
+						
+						#Case where a 7 is rolled
+						if(z == 7){
+							#Reduce cash based on bet
+							current_cash_1 <- current_cash_1 - bet_1
+							current_cash_2 <- current_cash_2 - bet_2
+							current_cash_3 <- current_cash_3 - bet_3
+							
+							#Update bets based on strategy
+							#Even wager strategy
+							if(current_cash_1 == 0){
+								bet_1 <- c(0)
+							}else{
+								bet_1 <- first_bet
+							}
+							#Martingale strategy
+							if(current_cash_2 == 0){
+								bet_2 <- c(0)
+							}else{
+								#If there is not enough cash to double the bet, bet remaining cash
+								if(current_cash_2<bet_2*2){
+									bet_2 <- current_cash_2
+								}else{
+									bet_2 <- bet_2*2
+								}
+							}
+							#Reverse Martingale strategy
+							if(current_cash_3 == 0){
+								bet_3 <- c(0)
+							}else{
+								bet_3 <- first_bet
+							}
+						}
+						
+						#If neither the point or a 7 is rolled, do nothing and roll again
+					}
+					
+					#Switch game flag
+					game_flag <- c(1)
+				}
+				#End of game
+			}
+			
+			#Print out results of game
+			cat(current_round, "\t\t", current_iteration, "\t\t\t", current_cash_1, "\t\t", current_cash_2, "\t\t", current_cash_3, "\n")
+		}
+		
+		#Write round results to file
+		row1<-t(c("Round ",current_round,":\n"))
+		row2<-t(c("Strategy\tNumber of Games\t\tEnding Balance (Dollars)","\n"))
+		row3<-t(c("1\t\t", num_games_1, "\t\t\t", current_cash_1,"\n"))
+		row4<-t(c("2\t\t", num_games_2, "\t\t\t", current_cash_2,"\n"))
+		row5<-t(c("3\t\t", num_games_3, "\t\t\t", current_cash_3,"\n"))
+		result<-data.frame(row1, row2, row3, row4, row5)
+		
+		write.table(result, file=outFile, append=TRUE, quote=FALSE, row.names=FALSE, col.names=FALSE, sep=" ")
+		#End of round
+	}
+	close(outFile)
+	cat("End of Program")
+#End of program
+}
